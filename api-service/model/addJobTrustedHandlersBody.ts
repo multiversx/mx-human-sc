56089886c72cf294cea476e686cdc8e496232d11
Claudiu-Marcel Bruda <claudiu725@yahoo.com>
2021-08-31 18:08:30 +0300

    Change to single quotes

--- api-service/model/addJobTrustedHandlersBody.ts
+++ api-service/model/addJobTrustedHandlersBody.ts
@@ -4,6 +4,6 @@ import { AddressDto } from "./addressDto";
 
 export class AddJobTrustedHandlersBody extends AddressDto {
     @IsArray()
-    @ApiProperty({ example: "[\"testAddress1\", \"testAddress2\"]", description: 'An array of addresses to be trusted' })
+    @ApiProperty({ example: '["testAddress1", "testAddress2"]', description: 'An array of addresses to be trusted' })
     handlers: Array<string>;
 }
